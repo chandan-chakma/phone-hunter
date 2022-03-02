dfc4de7d4f22060d569a7460144aef9266fe9309
chandan-chakma <cchakma19@gmail.com>
2022-03-02 14:25:42 +0600

    finall commit

--- js/phone.js
+++ js/phone.js
@@ -1,21 +1,36 @@
 // Add spinner  
 const toggleSpinn = displaySpin => {
     document.getElementById('spinner').style.display = displaySpin;
-
+}
+// display searching Phone after spinner load 
+const toggleSearchResult = displayresult => {
+    document.getElementById('search-result').style.display = displayresult;
 }
 
+// searchField 
 const loadPhones = () => {
     const searchField = document.getElementById('search-input');
     const searchText = searchField.value;
+
+    // // Error Message
+    const noFound = document.getElementById('no-found');
+    if (searchField.value == '') {
+        noFound.style.display = 'block'
+    }
+    else {
+        noFound.style.display = 'none'
+    }
+
     // display spinner  
     toggleSpinn('block');
-    console.log(searchText);
-    searchField.value = ''
+    toggleSearchResult('none');
 
+    // console.log(searchText);
+    searchField.value = '';
 
     // ----------------load Data -----------------
     const url = (` https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-    console.log(url);
+    // console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhones(data.data));
@@ -23,11 +38,14 @@ const loadPhones = () => {
 
 // display data phone name and brand
 const displayPhones = (phones) => {
-    console.log(phones);
+    // console.log(phones);
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
+
+    // error message 
+    const noFound = document.getElementById('no-found');
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone.slice);
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = ` 
@@ -43,35 +61,40 @@ const displayPhones = (phones) => {
         searchResult.appendChild(div);
 
     });
+
+
     // stop spinner  
     toggleSpinn('none');
+    toggleSearchResult('');
+
 }
 // load phone details id  
 const loadphoneDetails = id => {
-    console.log(id)
+    // console.log(id)
     const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhoneDetails(data.data));
 }
 
-
-
 // display phone details  
 const displayPhoneDetails = (phone) => {
-    console.log(phone);
+    // console.log(phone);
     const phoneDetails = document.getElementById('phone-detials');
+    phoneDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card')
     div.innerHTML = `
     <img src="${phone.image}" class="card-img-top w-75 p-4 mx-auto" alt="...">
     <div class="card-body">
-        <p class="card-text text-center fw-bold">Release-date: ${phone.releaseDate}</p>
-        <p class="card-text text-center fw-bold">Display: ${phone.mainFeatures.displaySize}</p>
-        <p class="card-text text-center fw-bold">Processor: ${phone.mainFeatures.chipSet}</p>
-        <p class="card-text text-center fw-bold">Storage: ${phone.mainFeatures.storage}</p>
-        <p class="card-text text-center fw-bold">Sensors: ${phone.mainFeatures.sensors}</p>
-        <p class="card-text text-center fw-bold">Other: ${phone?.others?.WLAN} <br> Bluetooth: ${phone?.others?.Bluetooth}<br>GPS: ${phone?.others?.GPS}<br> NFC: ${phone?.others?.NFC}<br>Radio: ${phone?.others?.Radio}<br>SBS: ${phone?.others?.SBS}</p>
+    <h4 class="fw-bolder">Features:</h4>
+        <p class="card-text fw-bold">Release-date: ${phone.releaseDate ? phone.releaseDate : 'no yet release'}</p>
+        <p class="card-text  fw-bold">Display: ${phone.mainFeatures.displaySize}</p>
+        <p class="card-text  fw-bold">Processor: ${phone.mainFeatures.chipSet}</p>
+        <p class="card-text  fw-bold">Storage: ${phone.mainFeatures.storage}</p>
+        <p class="card-text  fw-bold">Sensors: ${phone.mainFeatures.sensors}</p>
+        <h4 class="fw-bolder text-center">Others Features:</h4>
+        <p class="card-text text-center fw-bold">WLAN: ${phone.others?.WLAN ? phone.others.WLAN : 'NO'} <br> Bluetooth: ${phone.others?.Bluetooth ? phone.others.Bluetooth : 'NO'}<br>GPS: ${phone.others?.GPS ? phone.others.GPS : 'NO'}<br> NFC: ${phone.others?.NFC ? phone.others.NFC : 'NO'}<br>Radio: ${phone.others?.Radio ? phone.others.Radio : 'No'}<br>SBS: ${phone.others?.SBS ? phone.others.SBS : 'no'}</p>
         
             `;
     phoneDetails.appendChild(div);
